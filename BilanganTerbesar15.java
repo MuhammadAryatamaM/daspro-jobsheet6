42a743bef0997e8d09e1a7c4838e342073b8bb2d
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2024-10-04 09:40:06 +0700

    program tugas 1

---
+++ BilanganTerbesar15.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BilanganTerbesar15 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input15 = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("Masukkan bilangan pertama: ");
+        a = input15.nextInt();
+        System.out.print("Masukkan bilangan kedua: ");
+        b = input15.nextInt();
+        System.out.print("Masukkan bilangan ketiga: ");
+        c = input15.nextInt();
+
+        if ((a >= b) && (a >= c)) {
+            System.out.println(a + " adalah bilangan terbesar");
+        } else if ((b >= c) && (b >= a)) {
+            System.out.println(b + " adalah bilangan terbesar");
+        } else if ((c >= a) && (c >= b)) {
+            System.out.println(c + " adalah bilangan terbesar");
+        } else {
+            System.out.println("Input invalid");
+        }
+    }
+}
